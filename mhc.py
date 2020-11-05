89c71835f077950caf47f437d1e552a685ca772b
NAEK2K <naek2k@outlook.com>
2020-11-05 18:07:19 -0500

    optimization to file opening

--- mhc.py
+++ mhc.py
@@ -18,25 +18,25 @@ if not args.targets:
 if not os.path.exists(args.output):
     os.makedirs(args.output)
 
+header_output, head_output, footer_output, style_output = "", "", "", ""
+
+if args.header:
+    with open(args.header, "r") as f:
+        header_output = f.read()
+
+if args.head:
+    with open(args.head, "r") as f:
+        head_output = f.read()
+
+if args.footer:
+    with open(args.footer, "r") as f:
+        footer_output = f.read()
+
+if args.style:
+    with open(args.style, "r") as f:
+        style_output = f.read()
+
 for target in args.targets:
-    header_output, head_output, footer_output, style_output = "", "", "", ""
-
-    if args.header:
-        with open(args.header, "r") as f:
-            header_output = f.read()
-
-    if args.head:
-        with open(args.head, "r") as f:
-            head_output = f.read()
-
-    if args.footer:
-        with open(args.footer, "r") as f:
-            footer_output = f.read()
-
-    if args.style:
-        with open(args.style, "r") as f:
-            style_output = f.read()
-
     with open(args.output + "/" + target + ".html", "w+") as f:
         with open(target, "r") as g:
             html_output = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
