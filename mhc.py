ff427799bead98742bd8b5c0f60e9fe074e8a747
NAEK2K <naek2k@outlook.com>
2020-11-05 18:00:28 -0500

    added <head> support

--- mhc.py
+++ mhc.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(description="Arguments for MHC")
 parser.add_argument("--output", "-o", default="output")
 parser.add_argument("--targets", "-t", nargs="+")
 parser.add_argument("--header", "-hr")
+parser.add_argument("--head", "-hd")
 parser.add_argument("--footer", "-f")
 parser.add_argument("--style", "-s")
 args = parser.parse_args()
@@ -18,11 +19,15 @@ if not os.path.exists(args.output):
     os.makedirs(args.output)
 
 for target in args.targets:
-    header_output, footer_output, style_output = "", "", ""
+    header_output, head_output, footer_output, style_output = "", "", "", ""
 
     if args.header:
         with open(args.header, "r") as f:
             header_output = f.read()
+
+    if args.head:
+        with open(args.head, "r") as f:
+            head_output = f.read()
 
     if args.footer:
         with open(args.footer, "r") as f:
@@ -34,7 +39,15 @@ for target in args.targets:
 
     with open(args.output + "/" + target + ".html", "w+") as f:
         with open(target, "r") as g:
-            html_output = "{}\n{}\n{}\n{}".format(
-                header_output, markdown(g.read()), footer_output, style_output
+            html_output = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
+                "<html>",
+                head_output,
+                "<body>",
+                header_output,
+                markdown(g.read()),
+                footer_output,
+                "</body>",
+                style_output,
+                "</html>",
             )
             f.write(html_output)
